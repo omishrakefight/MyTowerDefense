0788374d8311d89debf45cf14e0ac84d2b47a31b
Kyle Genter <genter1993@live.com>
2018-06-04 23:25:11 -0400

    added title scene

---
+++ Tower_Reclamation/Assets/TitleScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Start!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GameStart()
+    {
+        print("function called");
+        SceneManager.LoadSceneAsync(1);
+
+    }
+}

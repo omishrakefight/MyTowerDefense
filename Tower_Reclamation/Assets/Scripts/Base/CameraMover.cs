6787c0d7fc07fa3ef6165c9244f2e0e4c1ce4952
Kyle Genter <genter1993@live.com>
2018-06-28 03:24:20 -0400

    added new scene Base
    Base is where you return after each mission.  Added a canvas manager to turn them all off except current.  Also added a camera move script that moves the camer to the different 'walls'.

---
+++ Tower_Reclamation/Assets/Scripts/Base/CameraMover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour {
+
+
+    [SerializeField] Camera camera;
+    Vector3 cameraLocation;
+
+    private void Start()
+    {
+        //camera.GetComponent<Camera>()
+        cameraLocation = camera.transform.position;
+    }
+
+    public void MoveCameraToComputer()
+    {
+        camera.transform.position = cameraLocation;
+    }
+    public void MoveCameraToTurrets()
+    {
+        camera.transform.position = cameraLocation + new Vector3(40, 0, 0);
+    }
+    public void MoveCameraToEngineerer()
+    {
+        camera.transform.position = cameraLocation + new Vector3(80, 0, 0);
+    }
+    public void MoveCameraToMeeting()
+    {
+        camera.transform.position = cameraLocation - new Vector3(40, 0, 0);
+    }
+    public void MoveCameraToTinker()
+    {
+        camera.transform.position = cameraLocation - new Vector3(80, 0, 0);
+    }
+
+}
---
+++ Tower_Reclamation/Assets/Scripts/Base/CanvasManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour {
+
+
+    [SerializeField] Canvas computerBase;
+    [SerializeField] Canvas turretFactory;
+    [SerializeField] Canvas engineerer;
+    [SerializeField] Canvas meetingRoom;
+    [SerializeField] Canvas generalUpgrades;
+    Canvas currentActive;
+
+    // Use this for initialization
+    void Start () {
+        turretFactory.gameObject.SetActive(false);
+        computerBase.gameObject.SetActive(false);
+        engineerer.gameObject.SetActive(false);
+        generalUpgrades.gameObject.SetActive(false);
+        meetingRoom.gameObject.SetActive(true);
+        currentActive = meetingRoom;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

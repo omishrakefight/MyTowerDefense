4adfde6259d87edf4210a6b8929c73bd6479187b
Kyle Genter <genter1993@live.com>
2019-03-30 16:05:29 -0400

    added pause menu

---
+++ Tower_Reclamation/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+
+    public static bool isPaused = false;
+
+    [SerializeField] GameObject pauseMenu;
+    // Use this for initialization
+    void Start() {
+        // starts unpaused.
+        Resume();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        print("loading the menu...");
+    }
+
+    public void QuitGame()
+    {
+        print("Quitting the game...");
+    }
+}

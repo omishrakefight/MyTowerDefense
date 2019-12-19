d5028514cce9cf086abacbcb706a693e66c3130a
Kyle Genter <35356483+omishrakefight@users.noreply.github.com>
2019-12-19 16:00:25 -0500

    starting UI updates, and started Tinker room string descriptions.
    Created afew pannels for UI improvements.  Also started brainstorming tinker room upgrades.  Mainly just names (ideas) and a short string description.

---
+++ Tower_Reclamation/Assets/Scripts/_new stuff/Tinker Upgrades.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TinkerUpgrades : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    // hmm not bad if I count on getting 10 of these with 4 upgrades each, 40 upgrades is not bad.
+
+    string silverWiring = "With a more conductive wiring, faster processes are enabled.  The tower can acquire targets and shoot at them faster.";
+    string alloyReasearchI = "By studying the art of alloy smelting, one can produce more quantity of the metals.  Increasing supply has lowered the cost.";
+    string studyTankI = "With research into more compressed tanks, we can put the product inside under more pressure, spraying in a farthur area.";
+    string heavyShellingI = "With study into the carpaces of the aliens, we have found out how to make the ballistics penetrate them more easily.";
+    string towerEngineerI = "Through a better study of towers, the ramping cost of continuous upgrades is cheaper.";
+    //string x = "";
+    //string x = "";
+    //string x = "";
+    //string x = "";
+    string scavengerI = "Through an edept scavenging eye, you start with more tower resources.";
+    string usableCart = "Finding a cart that was repairable, you can now cart some metal beams from site to site, increasing Base life.";
+    string rifleI = "In desperate situation, people will do anything.  But YOU found the rifle so its yours right?";
+    string tarI = "In desperation, people will do anything.  This shows them were the tar and fire is for when the aliens make it to your bases door.";
+
+    // check level layout and monster quantity.  earn this?
+    // maybe have final upgrades for if you complete a full set? like starting gold +++++ is start wiht a free tower?
+
+}

c75ef4505ad345b21b356b1ab0aa95d105b5b118
Kyle Genter <35356483+omishrakefight@users.noreply.github.com>
2020-09-02 13:53:19 -0400

    Made prefab for the Information promp (with picture)
    This should display an image, a text, pause time on start, and revert on close.  Close button destroys the object.

---
+++ Tower_Reclamation/Assets/MoreInformationPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoreInformationPanel : MonoBehaviour {
+
+    float outsideTimeScale;
+    float pauseTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+        outsideTimeScale = Time.timeScale;
+        Time.timeScale = pauseTime;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void CloseThePanel()
+    {
+        // revert it to whatever timescale it was before the popup.
+        Time.timeScale = outsideTimeScale;
+        Destroy(this.gameObject);
+    }
+}

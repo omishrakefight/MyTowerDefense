e83ed7325854ca2742f2eda00926d45b6a9d353e
Kyle Genter <35356483+omishrakefight@users.noreply.github.com>
2021-01-01 10:29:03 -0500

    sync

--- Tower_Reclamation/Assets/Scripts/Enemy scripts/EnemyHealth.cs
+++ Tower_Reclamation/Assets/Scripts/Enemy scripts/EnemyHealth.cs
@@ -205,7 +205,7 @@ public abstract class EnemyHealth : MonoBehaviour {
                 GetComponent<AudioSource>().PlayOneShot(enemyHitAudio);
             }
         }
-        else if {
+        else if () {
 
         }
 
